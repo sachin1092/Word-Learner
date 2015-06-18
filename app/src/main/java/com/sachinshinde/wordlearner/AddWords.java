b238f8e2ba21cad90a67b1c2308144b9f16fcd25
sachin1092 <me@sachinshinde.com>
2015-06-18 19:39:26 +0530

    tons of new features and new look. YO!

--- app/src/main/java/com/sachinshinde/wordlearner/AddWords.java
+++ app/src/main/java/com/sachinshinde/wordlearner/AddWords.java
@@ -76,6 +76,7 @@ public class AddWords extends Activity {
 //                    SerialPreference.savePrefs(getBaseContext(), words);
                     Utils.writeListToFile(mAdapter.getList(),Utils.WordsFile);
                     addToSessionList(et.getText().toString().trim());
+                    performItemClick(mAdapter.getPosition(et.getText().toString()));
                     et.setText("");
                     ((TextView) findViewById(R.id.tvCount)).setText("Total Count: " + mAdapter.getList().size() + " words");
                 }
@@ -83,149 +84,10 @@ public class AddWords extends Activity {
         });
 
         lvWords.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            AlertDialog dialog;
+
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
-                InputMethodManager imm = (InputMethodManager)getSystemService(
-                        Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(et.getWindowToken(), 0);
-                final AlertDialog.Builder builder = new AlertDialog.Builder(AddWords.this);
-                builder.setTitle(mAdapter.getItem(i));
-                ProgressBar pb = new ProgressBar(AddWords.this);
-                pb.setIndeterminate(true);
-
-                builder.setView(pb);
-                builder.setPositiveButton("Got It!", null);
-                new AsyncTask<String, Void, String>(){
-
-                    @Override
-                    protected String doInBackground(String... strings) {
-                        if(Utils.hasWord(strings[0])){
-                            Log.d("WordLearner", "Loading from memory");
-                            return Utils.getDefinition(strings[0]);
-                        }
-                        Log.d("WordLearner", "Loading from wordnik");
-                        return Utils.saveWord(strings[0], Utils.jsonToString(NetworkUtils.GET(Utils.URL1 + strings[0].trim().toLowerCase(Locale.US) + Utils.URL2)));
-                    }
-
-                    @Override
-                    protected void onPostExecute(String s) {
-
-                        builder.setView(null);
-                        builder.setMessage(s);
-                        builder.setNeutralButton("Speak", null);
-                        final AlertDialog mDialog = builder.create();
-                        mDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-
-
-
-                            @Override
-                            public void onShow(DialogInterface dialog) {
-
-                                final Button b = mDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
-
-                                final Handler handler = new Handler();
-                                final Runnable r = new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        handler.removeCallbacks(this);
-                                        if(done){
-                                            b.setText("Speak");
-                                            return;
-                                        }
-                                        handler.postDelayed(this, 250);
-                                        if(!b.getText().toString().contains(".")){
-                                            b.setText(".");
-                                        } else {
-                                            if(b.getText().toString().length() == 3){
-                                                b.setText(".");
-                                            } else {
-                                                b.setText(b.getText().toString() + ".");
-                                            }
-                                        }
-                                    }
-                                };
-
-                                b.setOnClickListener(new View.OnClickListener() {
-
-                                    @Override
-                                    public void onClick(View view) {
-
-                                        done = false;
-
-                                        handler.post(r);
-
-                                        new AsyncTask<String, Void, String>() {
-                                            @Override
-                                            protected void onPreExecute() {
-                                                super.onPreExecute();
-                                            }
-
-                                            @Override
-                                            protected String doInBackground(String... strings) {
-                                                String jsonString = NetworkUtils.GET("http://api.wordnik.com:80/v4/word.json/" + strings[0].trim().toLowerCase(Locale.getDefault()) + "/audio?useCanonical=false&limit=50&api_key=" + API_KEY);
-                                                try {
-                                                    JSONArray jsonArray = new JSONArray(jsonString);
-                                                    if (jsonArray.length() == 1) {
-                                                        return jsonArray.getJSONObject(0).getString("fileUrl");
-                                                    } else if(jsonArray.length() > 1){
-                                                        return jsonArray.getJSONObject(1).getString("fileUrl");
-                                                    } else {
-                                                        return null;
-                                                    }
-                                                } catch (JSONException e) {
-                                                    e.printStackTrace();
-                                                }
-
-                                                return null;
-                                            }
-
-                                            @Override
-                                            protected void onPostExecute(String s) {
-                                                super.onPostExecute(s);
-
-                                                if (s == null) {
-                                                    done = true;
-                                                    Toast.makeText(getBaseContext(), "No audio found", Toast.LENGTH_LONG).show();
-                                                } else {
-                                                    final MediaPlayer mp = new MediaPlayer();
-                                                    try {
-                                                        mp.setDataSource(s);
-                                                        mp.prepareAsync();
-                                                        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                                                            @Override
-                                                            public void onPrepared(MediaPlayer mp) {
-                                                                done = true;
-                                                                mp.start();
-                                                            }
-                                                        });
-                                                        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                                                            @Override
-                                                            public void onCompletion(MediaPlayer mediaPlayer) {
-                                                                if (mp != null)
-                                                                    mp.release();
-                                                            }
-                                                        });
-
-                                                    } catch (IOException e) {
-                                                    }
-                                                }
-
-
-                                            }
-                                        }.execute(mAdapter.getItem(i));
-
-                                    }
-                                });
-                            }
-                        });
-                        mDialog.show();
-                        dialog.cancel();
-                        super.onPostExecute(s);
-                    }
-                }.execute(mAdapter.getItem(i));
-                dialog = builder.create();
-                dialog.show();
+                performItemClick(i);
             }
         });
 
@@ -294,7 +156,153 @@ public class AddWords extends Activity {
         });
 
 
+    }
 
+    AlertDialog dialog;
+
+    public void performItemClick(final int position){
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(et.getWindowToken(), 0);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(AddWords.this);
+        builder.setTitle(mAdapter.getItem(position));
+        ProgressBar pb = new ProgressBar(AddWords.this);
+        pb.setIndeterminate(true);
+
+        builder.setView(pb);
+        builder.setPositiveButton("Got It!", null);
+        new AsyncTask<String, Void, String>(){
+
+            @Override
+            protected String doInBackground(String... strings) {
+                if(Utils.hasWord(strings[0])){
+                    Log.d("WordLearner", "Loading from memory");
+                    return Utils.getDefinition(strings[0]);
+                }
+                Log.d("WordLearner", "Loading from wordnik");
+                return Utils.saveWord(strings[0], Utils.jsonToString(NetworkUtils.GET(Utils.URL1 + strings[0].trim().toLowerCase(Locale.US) + Utils.URL2)));
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+
+                builder.setView(null);
+                builder.setMessage(s);
+                builder.setNeutralButton("Speak", null);
+                final AlertDialog mDialog = builder.create();
+
+                mDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+
+
+                    @Override
+                    public void onShow(DialogInterface dialog) {
+
+                        final Button b = mDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+
+                        final Handler handler = new Handler();
+                        final Runnable r = new Runnable() {
+                            @Override
+                            public void run() {
+                                handler.removeCallbacks(this);
+                                if(done){
+                                    b.setText("Speak");
+                                    return;
+                                }
+                                handler.postDelayed(this, 250);
+                                if(!b.getText().toString().contains(".")){
+                                    b.setText(".");
+                                } else {
+                                    if(b.getText().toString().length() == 3){
+                                        b.setText(".");
+                                    } else {
+                                        b.setText(b.getText().toString() + ".");
+                                    }
+                                }
+                            }
+                        };
+
+                        b.setOnClickListener(new View.OnClickListener() {
+
+                            @Override
+                            public void onClick(View view) {
+
+                                done = false;
+
+                                handler.post(r);
+
+                                new AsyncTask<String, Void, String>() {
+                                    @Override
+                                    protected void onPreExecute() {
+                                        super.onPreExecute();
+                                    }
+
+                                    @Override
+                                    protected String doInBackground(String... strings) {
+                                        String jsonString = NetworkUtils.GET("http://api.wordnik.com:80/v4/word.json/" + strings[0].trim().toLowerCase(Locale.getDefault()) + "/audio?useCanonical=false&limit=50&api_key=" + API_KEY);
+                                        try {
+                                            JSONArray jsonArray = new JSONArray(jsonString);
+                                            if (jsonArray.length() == 1) {
+                                                return jsonArray.getJSONObject(0).getString("fileUrl");
+                                            } else if(jsonArray.length() > 1){
+                                                return jsonArray.getJSONObject(1).getString("fileUrl");
+                                            } else {
+                                                return null;
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                        return null;
+                                    }
+
+                                    @Override
+                                    protected void onPostExecute(String s) {
+                                        super.onPostExecute(s);
+
+                                        if (s == null) {
+                                            done = true;
+                                            Toast.makeText(getBaseContext(), "No audio found", Toast.LENGTH_LONG).show();
+                                        } else {
+                                            final MediaPlayer mp = new MediaPlayer();
+                                            try {
+                                                mp.setDataSource(s);
+                                                mp.prepareAsync();
+                                                mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                                                    @Override
+                                                    public void onPrepared(MediaPlayer mp) {
+                                                        done = true;
+                                                        mp.start();
+                                                    }
+                                                });
+                                                mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                                                    @Override
+                                                    public void onCompletion(MediaPlayer mediaPlayer) {
+                                                        if (mp != null)
+                                                            mp.release();
+                                                    }
+                                                });
+
+                                            } catch (IOException e) {
+                                            }
+                                        }
+
+
+                                    }
+                                }.execute(mAdapter.getItem(position));
+
+                            }
+                        });
+                    }
+                });
+                mDialog.show();
+                dialog.cancel();
+                super.onPostExecute(s);
+            }
+        }.execute(mAdapter.getItem(position));
+        dialog = builder.create();
+        dialog.show();
     }
 
     boolean done = false;
--- app/src/main/java/com/sachinshinde/wordlearner/MyActivity.java
+++ app/src/main/java/com/sachinshinde/wordlearner/MyActivity.java
@@ -2,33 +2,62 @@ package com.sachinshinde.wordlearner;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 
-public class MyActivity extends Activity {
+public class MyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.word_learner);
+        ab.setTitle("");
+//        ab.setDisplayHomeAsUpEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
         findViewById(R.id.buttonAddWords).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,8 +105,114 @@ public class MyActivity extends Activity {
             }
         }
 
+        findViewById(R.id.buttonImport).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
     }
 
+    private static final int FILE_SELECT_CODE = 0;
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    Log.d("Word Learner", "File Uri: " + uri.toString());
+                    // Get the path
+                    String path = null;
+                    try {
+                        path = getPath(this, uri);
+                        Toast.makeText(getBaseContext(), path, Toast.LENGTH_LONG).show();
+                        ArrayList<String> newWordList = parseFile(path);
+                        ArrayList<String> oldList = Utils.loadListFromFile(Utils.WordsFile);
+                        if(oldList != null)
+                            newWordList.addAll(oldList);
+
+
+                        // add elements to al, including duplicates
+                        Set<String> hs = new HashSet<>();
+                        hs.addAll(newWordList);
+                        newWordList.clear();
+                        newWordList.addAll(hs);
+
+                        Utils.writeListToFile(newWordList, Utils.WordsFile);
+
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d("Word Learner", "File Path: " + path);
+                    // Get the file instance
+                    // File file = new File(path);
+                    // Initiate the upload
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static String getPath(Context context, Uri uri) throws URISyntaxException {
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = { "_data" };
+            Cursor cursor = null;
+
+            try {
+                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow("_data");
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                // Eat it
+            }
+        }
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public ArrayList<String> parseFile(String path){
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                list.add(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 
     public class ExportWords extends AsyncTask<String, Void, String> {
 
@@ -135,6 +270,13 @@ public class MyActivity extends Activity {
 
             return null;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_actions, menu);
+        return true;
     }
 
 }
